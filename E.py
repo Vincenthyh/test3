bbfbe1e2b9b5ecbc7a10e7933aa7868067eef0d8
Vincenthyh <3029130076@qq.com>
2025-12-23 19:17:46 +0800

    yes

---
+++ E.py
@@ -0,0 +1,8 @@
+t=int(input())
+for _ in range(t):
+    n,k=map(int,input().split())
+    a=list(map(int,input().split()))
+    if not isinstance(int,n/k):
+        print(0)
+    else:
+        pass 
